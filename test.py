5a3d3b229d8d479f25615e0fb92ea944a3879d50
HKAB <khoahockithuatmc@gmail.com>
2022-12-29 15:03:09 +0700

    add test.wav, voskapi structure

---
+++ test.py
@@ -0,0 +1,26 @@
+from vosk import Model, KaldiRecognizer
+import wave
+import sys
+
+wf = wave.open("test.wav", "rb")
+if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
+    print("Audio file must be WAV format mono PCM.")
+    sys.exit(1)
+
+# fbank.conf in https://github.com/alphacep/vosk-api/blob/master/src/model.cc
+model = Model("my_model")
+
+rec = KaldiRecognizer(model, wf.getframerate())
+rec.SetWords(True)
+rec.SetPartialWords(True)
+
+while True:
+    data = wf.readframes(4000)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        print(rec.Result())
+    else:
+        print(rec.PartialResult())
+
+print(rec.FinalResult())
